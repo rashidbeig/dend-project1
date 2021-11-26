368e2b8548d2afc1bfcd387fa3a165ab2f5dce4d
root <rashid.beig@gmail.com>
2021-11-26 17:11:47 +0000

    third commit

--- .ipynb_checkpoints/sql_queries-checkpoint.py
+++ .ipynb_checkpoints/sql_queries-checkpoint.py
@@ -8,7 +8,18 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-
+songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays
+    (songplay_id SERIAL PRIMARY KEY, 
+    start_time TIMESTAMP NOT NULL, 
+    user_id INT NOT NULL, 
+    level VARCHAR NOT NULL, 
+    song_id VARCHAR, 
+    artist_id VARCHAR, 
+    session_id INTEGER NOT NULL, 
+    location VARCHAR NOT NULL, 
+    user_agent VARCHAR NOT NULL)
+""")
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users
@@ -19,97 +30,105 @@ user_table_create = ("""
     level VARCHAR)
 """)
 
-artist_table_create = ("""
-    CREATE TABLE IF NOT EXISTS artists
-    (artist_id VARCHAR PRIMARY KEY,
-     name VARCHAR NOT NULL, 
-     location VARCHAR, 
-     lattitude float, 
-     longitude float)
-""")
-
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS songs
     (song_id VARCHAR PRIMARY KEY, 
     title VARCHAR NOT NULL, 
-    artist_id VARCHAR, 
-    year INTEGER, 
+    artist_id VARCHAR NOT NULL, 
+    year INTEGER NOT NULL, 
     duration float NOT NULL)
 """)
 
+artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists
+    (artist_id VARCHAR PRIMARY KEY,
+     name VARCHAR NOT NULL, 
+     location VARCHAR NOT NULL, 
+     latitude float, 
+     longitude float)
+""")
 
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time
-    (start_time TIMESTAMP PRIMARY KEY,
-     hour INTEGER, 
-     day INTEGER, 
-     week INTEGER, 
-     month INTEGER, 
-     year INTEGER, 
-     weekday VARCHAR)
+    (start_time TIMESTAMP NOT NULL,
+     hour INTEGER NOT NULL, 
+     day INTEGER NOT NULL, 
+     week INTEGER NOT NULL, 
+     month INTEGER NOT NULL, 
+     year INTEGER NOT NULL, 
+     weekday INTEGER NOT NULL)
 """)
 
-songplay_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songplays
-    (songplay_id SERIAL PRIMARY KEY, 
-    start_time TIMESTAMP REFERENCES time(start_time), 
-    user_id int NOT NULL REFERENCES users(user_id), 
-    level VARCHAR, 
-    song_id VARCHAR REFERENCES songs(song_id), 
-    artist_id VARCHAR REFERENCES artists(artist_id), 
-    session_id INTEGER, 
-    location VARCHAR, 
-    user_agent VARCHAR)
-""")
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-    INSERT INTO songplays
-    (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-    ON CONFLICT (songplay_id) DO NOTHING;
+INSERT INTO songplays
+    (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES
+    (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
-    INSERT INTO users
+INSERT INTO users
     (user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING;
+VALUES
+    (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO UPDATE
+    SET level = excluded.level
+""")
+
+
+song_table_insert = ("""
+INSERT INTO songs
+    (song_id, title, artist_id, year, duration)
+VALUES
+    (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""
-    INSERT INTO artists
-    (artist_id, name, location, lattitude, longitude)
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (artist_id) DO NOTHING;
-""")
-
-song_table_insert = ("""
-    INSERT INTO songs
-    (song_id, title, artist_id, year, duration)
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (song_id) DO NOTHING;
+INSERT INTO artists
+    (artist_id, name, location, latitude, longitude)
+VALUES
+    (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING
 """)
 
 time_table_insert = ("""
-    INSERT INTO time
+INSERT INTO time
     (start_time, hour, day, week, month, year, weekday)
-    VALUES (%s, %s, %s, %s, %s, %s, %s)
-    ON CONFLICT (start_time) DO NOTHING;
+VALUES
+    (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
-    SELECT song_id, artists.artist_id
-    FROM songs JOIN artists ON songs.artist_id = artists.artist_id
-    WHERE songs.title = %s
-    AND artists.name = %s
-    AND songs.duration = %s
+SELECT
+    s.song_id, s.artist_id
+FROM
+    songs s
+        JOIN artists a ON s.artist_id = a.artist_id
+WHERE
+    s.title = %s
+    AND a.name = %s
+    AND s.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
-drop_table_queries = [user_table_drop, artist_table_drop, song_table_drop, time_table_drop, songplay_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
